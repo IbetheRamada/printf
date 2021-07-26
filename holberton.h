6b2130c53b9f8edd99702cbd39c6b4dcbb768858
IbetheRamada <valerdagd@gmail.com>
2021-07-26 12:07:27 -0500

    remove flowchart

--- holberton.h
+++ holberton.h
@@ -1,10 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/*------------------*/
+
 #include <stdio.h>
 #include <stdarg.h>
 
+/*------------------*/
+
 int _putchar(char c);
 int _printf(const char *format, ...);
-
+/*------------------*/
 #endif /* Holberton_h */
