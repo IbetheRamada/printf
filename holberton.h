5e6452f38eddcfba4ffe23663e8728e9e76667ec
IbetheRamada <valerdagd@gmail.com>
2021-07-26 16:49:58 -0500

    add identifier file

--- holberton.h
+++ holberton.h
@@ -4,11 +4,18 @@
 /*------------------*/
 
 #include <stdio.h>
+#include <string.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /*------------------*/
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int variable_identifier();
+int type_of_variable();
+
+
 /*------------------*/
 #endif /* Holberton_h */
